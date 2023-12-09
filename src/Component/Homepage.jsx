74489b430c19dbc64d7071e1baed9b4d6e98b914
ayabakry <ayatbakry@yahoo.com>
2023-12-09 13:09:51 +0200

    M1

--- src/Component/Homepage.jsx
+++ src/Component/Homepage.jsx
@@ -1,9 +1,34 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from "react-router-dom";
 import "../Styling/Homepage.scss"
 import Logo from "../Imgs/BankMisr.jpg"
 function HomePage() {
+    const [Convert, setConvert] = useState({
+
+        AmountInput: "",
+
+    })
+    const [errors, setErrors] = useState({
+        AmountInputErr: true,
+
+    })
+    const Converter = (e) => {
+        if (e.target.name === "amountNum") {
+            const valId = /^[0-9\b]+$/;
+            setConvert({
+                ...Convert,
+                AmountInput: e.target.value
+            })
+            if (e.target.value.length === 0 || !(valId.test(e.target.value))) {
+                setErrors({ ...errors, AmountInputErr: true })
+            } else {
+                setErrors({ ...errors, AmountInputErr: false })
+            }
+
+
+        }
+    }
     return (
         <Fragment>
             <nav className="navbar navbar-expand-lg navbar-light " >
@@ -11,7 +36,7 @@ function HomePage() {
 
                     <Link className="navbar-brand" to="/">
 
-                    <img src={Logo} alt="" width="100" height="50"/>
+                        <img src={Logo} alt="" width="100" height="50" />
                     </Link>
                     <form className="d-flex">
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,15 +60,20 @@ function HomePage() {
 
 
                 <label for="subject2" className="form-label" >Amount </label>
-                <input type="text" id="subject2" className="form-control" style={{ margin: '0px 0px 10px 25px', width: '10%' }}></input>
-                {/* <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example"
-                    style={{margin:'10px 0px 10px 0px', width:'10%'}}
-                    >
-                        <option selected>EUR</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select> */}
+                <input type="text" id="subject2" className=
+                    {`form-control ${errors.AmountInputErr ? "border-primary" : "border-primary"}`}
+                    name="amountNum"
+                    placeholder='Enter Your Amount'
+                    onChange={(e) => Converter(e)}
+                    value={Convert.AmountInput}
+                    style={{ margin: '0px 0px 10px 25px', width: '10%' }}>
+
+                </input>
+                <p className="text-danger">
+                    {errors.AmountInput ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style={{ width: '3%', marginLeft: '5px' }} fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" /> <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" /> </svg> : ' '}
+
+                    {errors.AmountInput ? 'Enter Amount Only Number' : ''} </p>
+
                 <label for="subject2" className="form-label" >From  </label>
                 <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example"
                     style={{ margin: '0px 0px 10px 25px', width: '10%' }}
@@ -62,7 +92,8 @@ function HomePage() {
                     <option value="2">Two</option>
                     <option value="3">Three</option> */}
                 </select>
-                <button type="button" class="btn btn-primary">Convert</button>
+                <button type="button" class="btn btn-primary" disabled={!errors.AmountInputErr  ? false : true}
+                >Convert</button>
                 <input type="text" className="form-control" style={{ margin: '0px 0px 10px 25px', width: '10%' }}></input>
 
             </div>
